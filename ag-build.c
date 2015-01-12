d9d50ca88b78a2c6831eba03424c431a032783d0
Dmitry Savenko <dsavenko@xored.com>
2015-01-12 22:55:24 +0600

    Directly specify components to build. Fix issue with agnostic not dying after build failure

--- ag-build.c
+++ ag-build.c
@@ -45,18 +45,30 @@ static void build_component(struct ag_project* project, struct ag_component* c) 
     }
     char* parent_dir = ag_component_dir(project, c);
     if (!parent_dir) {
+        remove(script);
         die("Unable to find parent directory of the component.");
     }
     debug_print("Running script %s from parent directory %s\n", script, parent_dir);
     pid_t child_pid = run_script(parent_dir, script);
     if (-1 == child_pid) {
         perror(NULL);
+        remove(script);
         die("Failed to run build");
     }
+
     int status = 0;
     wait(&status);
 
     remove(script);
+
+    if (WIFEXITED(status)) {
+        if (WEXITSTATUS(status)) {
+            die("Failed to build %s", c->name);
+        }
+    } else {
+        die("Build interrupted: %s", c->name);
+    }
+
     free(script);
     free(parent_dir);
 }
@@ -65,8 +77,17 @@ static void build_current(struct ag_project* project) {
     build_component(project, extract_component(project, 0, NULL));
 }
 
-static void build_single(struct ag_project* project, int argc, const char** argv) {
-    build_component(project, extract_component(project, argc, argv));
+static void build_list(struct ag_project* project, int argc, const char** argv) {
+    while (0 < argc) {
+        struct ag_component* c = ag_find_component(project, *argv);
+        // TODO: re-write this to support -c and use extract_component()
+        if (!c) {
+            die("Component not found: %s", *argv);
+        }
+        --argc;
+        ++argv;
+        build_component(project, c);
+    }
 }
 
 static void build_up(struct ag_project* project, int argc, const char** argv) {
@@ -95,7 +116,7 @@ void build(int argc, const char** argv) {
         if (!strcmp("up", *argv)) {
             build_up(project, argc-1, argv+1);
         } else {
-            build_single(project, argc, argv);
+            build_list(project, argc, argv);
         }
     } else {
         build_current(project);
